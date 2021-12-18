1df103e13b26013e65a4aa2868bebfb46bb92b9c
keitaogitsu <ogitsukeita@gmail.com>
2021-12-18 16:05:59 +0900

    削除処理実装まで

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,9 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+        
+        <script src="https://kit.fontawesome.com/700f927bd6.js" crossorigin="anonymous"></script>
+        
         
     </head>
     <body>
@@ -26,6 +28,23 @@
         <div class='footer'>
             <a href="/">戻る</a>
             <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+            <form action="/posts/{{ $post->id }}" id="form" method="post" style="display:inline">
+                @csrf
+                @method('DELETE')
+                <span onclick="Check(this);"><i class="fa fa-trash" aria-hidden="true"></i></span> 
+                <script type="text/javascript">
+                    function Check(e){
+                        let checked = confirm("削除しますか？");
+                        if (checked==true) {
+                            document.getElementById("form").submit();
+                        } else if (checked==false) {
+                            console.log('false if is executed');
+
+                            return false;
+                        }
+                    }
+                </script>
+            </form>
         </div>
     </body>
 </html>
